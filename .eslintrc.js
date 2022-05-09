288584d23ecc9259fc87086739ee61c182d19dcf
Fogrex <fogrexon@gmail.com>
2022-05-09 22:31:12 +0900

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    // 'plugin:react/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/member-delimiter-style": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  plugins: ["react"],
+  overrides: [
+    {
+      // 直下のファイル
+      files: ["*.{js,mjs,cjs}"],
+      excludedFiles: ["*/**/*.{js,mjs,cjs}"],
+      env: {
+        node: true,
+      },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  reportUnusedDisableDirectives: true,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const cpctfBaseUrl = "https://cpctf.space";
+
 export default defineConfig({
-  plugins: [react()]
-})
+  base: process.env.BUILD_BASE_PATH || "/",
+  build: {
+    outDir: "./build",
+  },
+  resolve: {
+    alias: {
+      "#/": `${__dirname}/src/`,
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: cpctfBaseUrl,
+        changeOrigin: true,
+      },
+      "/ws": {
+        target: cpctfBaseUrl,
+        changeOrigin: true,
+        ws: true,
+        headers: {
+          origin: cpctfBaseUrl,
+        },
+      },
+      "/images": {
+        target: cpctfBaseUrl,
+        changeOrigin: true,
+      },
+      "/visualizer/circuit.json": {
+        target: cpctfBaseUrl,
+        changeOrigin: true,
+      },
+    },
+  },
+});
